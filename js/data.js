a11204e24144b7e59783522138e73c9facd54779
mode12345678 <54635738+mode12345678@users.noreply.github.com>
2019-10-03 20:22:32 +0300

    3. Visų mėnesių pavadinimai

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 const account = [
-    { month: 0, income: 150 },
+    { month: 10, income: 150 },
     { month: 1, income: 200, expense: 50 },
     { month: 3, income: 450, expense: 200 },
     { month: 7, expense: 50 },
@@ -13,4 +13,4 @@ const account = [
     { month: 12, expense: 200 }
 ];
 
-const months = ['Sausis', 'Vasaris', 'Kovas'];
+const months = ['Sausis', 'Vasaris', 'Kovas','Balandis', 'Gegužė', 'Birželis','Liepa', 'Rugpjutis', 'Rusejis','Spalis', 'Lapkritis', 'Gruodis',];
---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function renderMonths( data ) {
+let = HTML '';
+
+for ( let i=0; i<12; i++ ) {
+    HTML += 
+}
+
+}
